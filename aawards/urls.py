ec626d845c05b7cffff707be42586dd9751b4d83
seron-ux <abyseronkirui@gmail.com>
2021-04-04 18:49:19 +0300

    registered the urlpattern

--- aawards/urls.py
+++ aawards/urls.py
@@ -5,4 +5,5 @@ from . views import index
 urlpatterns = [
     path('',index, name='home'),
     path('register/',views.register, name='registration'),
+    path('search/', views.searchprofile, name='search'),
 ]
