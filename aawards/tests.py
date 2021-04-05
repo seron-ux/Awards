e5712fc0468227f4aea895203f6ae08bd3dced4d
seron-ux <abyseronkirui@gmail.com>
2021-04-05 22:29:23 +0300

    made changes on email

--- aawards/tests.py
+++ aawards/tests.py
@@ -5,23 +5,23 @@ from .models import Profile,Projects
 from django.contrib.auth.models import User
 
 
-# class ProfileTest(TestCase):
-#     def setUp(self):
-#         self.dorcas = User(username = 'Dorcas',email = 'dorcasmosonik@gmail.com')
-#         self.dorcas = Profile(user = Self.dorcas,user = 1,Bio = 'tests',photo = 'test.jpg',date_craeted='dec,01.2020')
+class ProfileTest(TestCase):
+    def setUp(self):
+        self.Abigael = User(username = 'Abigael',email = 'abyseronkirui@gmail.com)
+        self.Abigael = Profile(user = Self.dorcas,user = 1,Bio = 'tests',photo = 'test.jpg',date_craeted='dec,01.2020')
 
-#     def test_instance(self):
-#         self.assertTrue(isinstance(self.dorcas,Profile))
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Abigael,Profile))
 
-#     def test_save_profile(self):
-#         Profile.save_profile(self)
-#         all_profiles = Profile.objects.all()
-#         self.assertTrue(len(all_profiles),0)
+    def test_save_profile(self):
+        Profile.save_profile(self)
+        all_profiles = Profile.objects.all()
+        self.assertTrue(len(all_profiles),0)
 
-#     def test_delete_profile(self):
-#         self.dorcas.delete_profile()
-#         all_profiles = Profile.objects.all()
-#         self.assertEqual(len(all_profiles),0)
+    def test_delete_profile(self):
+        self.dorcas.delete_profile()
+        all_profiles = Profile.objects.all()
+        self.assertEqual(len(all_profiles),0)
 
 
 
